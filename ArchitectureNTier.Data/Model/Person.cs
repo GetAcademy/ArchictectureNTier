be40d49256120334b6cbfcbc5e1157bf08c23ce0
GET Academy <terje@kolderup.net>
2020-01-22 12:02:19 +0100

    x

---
+++ ArchitectureNTier.Data/Model/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ArchitectureNTier.Data.Model
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public DateTime? DeathDate { get; set; }
+
+        public int? Father { get; set; }
+        public int? Mother { get; set; }
+
+    }
+}
---
+++ ArchitectureNTier.Data/PersonRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+using ArchitectureNTier.Data.Model;
+using Dapper;
+
+namespace ArchitectureNTier.Data
+{
+    public class PersonRepository : Repository<Person>
+    {
+        public PersonRepository(string connectionString) : base(connectionString)
+        {
+        }
+
+        public async Task<IEnumerable<Person>> ReadChildren(int parentId)
+        {
+            var whereClause = " Father = @Id OR Mother = @Id ";
+            return await ReadMany(whereClause, new { Id = parentId});
+        }
+    }
+}
