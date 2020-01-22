a9f272d295bffa49dbc58b431476a53d37c9c174
GET Academy <terje@kolderup.net>
2020-01-22 12:16:53 +0100

    x

---
+++ ArchictectureNTier/Controllers/PersonController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ArchitectureNTier.Logic;
+using ArchitectureNTier.Logic.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArchictectureNTier.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        // GET: api/Person
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Person/5
+        [HttpGet("{id}", Name = "Get")]
+        public async Task<Person> Get(int id)
+        {
+            var service = new PersonService();
+            return await service.GetPerson(id);
+        }
+
+        // POST: api/Person
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Person/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
